77d2299544bebd9953cbeb461440eaedff155bba
hosnipogi <hosnibona@gmail.com>
2021-10-28 06:26:18 +0800

    added max number of running positions check

--- src/helpers/hasOpenPosition.ts
+++ src/helpers/hasOpenPosition.ts
@@ -5,6 +5,14 @@ export default async ({ api, symbol }: { api: Binance; symbol: string }) => {
     const allPositions = await api.futuresPositionRisk();
     if (!allPositions) throw "Cannot GET allPositions";
 
+    const maxRunningPositions = +process.env.MAXRUNNINGPOSITIONS!;
+    const runningPositions: [] = allPositions.filter(
+        ({ positionAmt }: { positionAmt: number }) => +positionAmt !== 0
+    );
+
+    if (runningPositions.length >= maxRunningPositions)
+        throw `Max number of running Positions open max: ${maxRunningPositions}`;
+
     const found = allPositions.find(
         (data: { symbol: string }) => data.symbol === symbol
     );
